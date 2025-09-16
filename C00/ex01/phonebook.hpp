f59fd75d9425c5adebaa78fc4132382d60436e64
RaphyStoll <raphael.ferreira@ik.me>
2025-09-16 11:32:41 +0200

    debut c01

---
+++ C00/ex01/phonebook.hpp
@@ -0,0 +1,26 @@
+#ifndef PHONEBOOK.HPP
+# define PHONEBOOK.HPP
+
+#include "contact.hpp"
+
+class PhoneBook {
+	private :
+		Contact	contact[8];
+		int		count; // conteur de plage pleine 1 -> 8
+		int		next; //index 0 -> 7
+	public :
+		PhoneBook();
+		PhoneBook(const PhoneBook& other);
+		PhoneBook& operator=(const PhoneBook phoneBook); //Right-Hand Side ou celui qu'on modifie (standard)
+		~PhoneBook();
+
+	void add(const Contact& contact);
+	int  size() const;
+
+	int findByFirstName(const std::string& name) const;
+    int findByLastName(const std::string& name) const;
+    int findByNickName(const std::string& name) const;
+
+};
+
+#endif
