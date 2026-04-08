f8f072b1a19b1271357245b4a1c9f0170dbac265
RaphyStoll <raphael.ferreira@ik.me>
2026-04-08 19:17:57 +0200

    Refactor PmergeMe class: improve constructor validation and enhance vector handling

--- CPP09/ex02/PmergeMe.cpp
+++ CPP09/ex02/PmergeMe.cpp
@@ -1,5 +1,5 @@
-#include "libftpp.hpp"
 #include "PmergeMe.hpp"
+#include "libftpp.hpp"
 
 #include <algorithm>
 #include <exception>
@@ -7,95 +7,55 @@
 #include <ostream>
 #include <vector>
 
-PmergeMe::PmergeMe(int argc, char **tab) : _logger("general")
-{
-	if (!init_vector(argc, tab))
-		throw PmergeMe::ParseError();
-	_size = _v.size();
-	if (!is_sorted(get_v())) {
-		_logger << "in constructor isn't sorted" << std::endl;
-		throw PmergeMe::StartSorted(); 
-	}
-	_start_time = libftpp::time::Clock::now_us();
-	_end_time = 0;
-	std::cout << "before : ";
-	print_vector();
+PmergeMe::PmergeMe(int argc, char **tab) : _logger("general") {
+  if (!init_vector(argc, tab))
+    throw PmergeMe::ParseError();
+  if (!is_sorted(get_v())) {
+    _logger << "in constructor is sorted" << std::endl;
+    throw PmergeMe::StartSorted();
+  }
+  if (!check_duplicate_value(get_v())) {
+    _logger << "dobble value not accepted" << std::endl;
+    throw PmergeMe::ParseError();
+  }
+  _start_time = libftpp::time::Clock::now_us();
+  _end_time = 0;
+  std::cout << "before : ";
+  print_vector();
 }
 
-bool PmergeMe::init_vector(int argc, char **tab)
-{
-	_logger << pad_line("init vector");
-	for (int i = 1; i < argc; i++) {
-		try {
-			int a = libftpp::str::StringUtils::stoi(tab[i]);
-			if (a < 0)
-				return false;
-			_v.push_back(a);
-		}
-		catch (std::invalid_argument &e) {
-			_logger << "argv[" << i << "] = " << "'" << tab[i] << "'" << " is invalid" << std::endl;
-			return false;
-		}
-		catch (std::out_of_range &e) {
-			std::cerr << "out of range int MIN|MAX" << std::endl;
-			return false;
-		}
-	}
-	return true;
+bool PmergeMe::init_vector(int argc, char **tab) {
+  _logger << pad_line("init vector");
+  for (int i = 1; i < argc; i++) {
+    try {
+      int a = libftpp::str::StringUtils::stoi(tab[i]);
+      if (a < 0)
+        return false;
+      _v.push_back(a);
+    } catch (std::invalid_argument &e) {
+      _logger << "argv[" << i << "] = " << "'" << tab[i] << "'" << " is invalid"
+              << std::endl;
+      return false;
+    } catch (std::out_of_range &e) {
+      std::cerr << "out of range int MIN|MAX" << std::endl;
+      return false;
+    }
+  }
+  return true;
 }
 
-void PmergeMe::print_vector()
-{
-	for(size_t i = 0; i < _v.size(); i++) {
-		_logger << _v[i] << " ";
-		std::cout << _v[i] << " ";
-	}
-	_logger << std::endl;
-	std::cout << std::endl;
+void PmergeMe::Merge_sort(std::vector<int> vct) {
+
+  if (!PmergeMe::is_sorted(vct))
+    throw PmergeMe::NotSorted();
+  _logger << "sorted" << std::endl;
 }
 
-bool PmergeMe::is_sorted(const std::vector<int> &vct)
-{
-	if (vct.size() < 2)
-		return true;
-	for (size_t i = 1; i < vct.size(); ++i) {
-		if (vct[i - 1] > vct[i]) {
-			_logger << "isn't sorted = " << vct[i - 1] << ">" << vct[i] << std::endl;
-			return false;
-		}
-	}
-	return true;
-}
-
-void PmergeMe::Merge_sort(std::vector<int> vct){
-
-	if(!PmergeMe::is_sorted(vct))
-		throw PmergeMe::NotSorted();
-	_logger << "sorted" << std::endl;
-}
-
-void PmergeMe::run()
-{
-	PmergeMe::Merge_sort(PmergeMe::get_v());
-	std::cout << "after : ";
-	PmergeMe::print_vector();
-	//Time to process a range of 5 elements with std::[..] : 0.00031 us
-	PmergeMe::print_time(_v);
-	return;
-}
-
-
-const char *PmergeMe::ParseError::what() const throw()
-{
-	return "Parsing error look logs";
-}
-
-const char *PmergeMe::NotSorted::what() const throw()
-{
-	return "this isn't sorted";
-}
-
-const char *PmergeMe::StartSorted::what() const throw()
-{
-	return "It's sorted";
+void PmergeMe::run() {
+  PmergeMe::Merge_sort(PmergeMe::get_v());
+  std::cout << "after : ";
+  PmergeMe::print_vector();
+  // Time to process a range of 5 elements with std::[..] : 0.00031 us
+  PmergeMe::print_time(_v);
+  return;
 }
--- CPP09/ex02/PmergeMe.hpp
+++ CPP09/ex02/PmergeMe.hpp
@@ -1,58 +1,135 @@
 #include "libftpp.hpp"
+#include <deque>
 #include <exception>
 #include <iostream>
-#include <vector> 
+#include <utility>
+#include <vector>
 
 class PmergeMe {
-	private:
-		std::vector<int> _v;
-		libftpp::debug::DebugLogger _logger;
-		int _size;
-		libftpp::time::timestamp_us_t _start_time;
-		libftpp::time::timestamp_us_t _end_time;
+private:
+  libftpp::debug::DebugLogger _logger;
 
-		bool init_vector(int argc,char **argv);
-		void Merge_sort(std::vector<int> vct);
+  std::vector<int> _v;
+  std::vector<std::pair<int, int> > _pairs;
+  std::deque<int> _chain_deque;
+  std::vector<int> _chain_vector;
+  int _stash;
 
-		bool is_sorted(const std::vector<int> &vct);
-		void print_vector();
+  libftpp::time::timestamp_us_t _start_time;
+  libftpp::time::timestamp_us_t _end_time;
 
-		template <typename T>
-		void print_time(const T &inspected)
-		{
-			std::cout	<< "Time to process a range of "
-						<< get_size()
-						<< " elements with " << libftpp::meta::TypeInspector::name(inspected) << " : "
-						<< libftpp::time::Clock::elapsed_compact_since_us(_start_time) << std::endl;
-		}
-	public:
-		PmergeMe(int argv, char **tab); 
-		~PmergeMe() {};
-		PmergeMe(const PmergeMe& other) : _v(other._v), _logger(other._logger) {};
-		PmergeMe& operator=(const PmergeMe& other) {
-			if (this != &other) {
-				_v = other._v;
-				_logger = other._logger;
-			}
-			return *this;
-		}
-		libftpp::time::timestamp_us_t get_start_size() { return  this->_start_time;};
-		libftpp::time::timestamp_us_t get_end_time() { return this->_end_time;};
-		std::vector<int> get_v() { return this->_v;};
-		int get_size() { return this->_size;};
-		
-		void run();
+  bool init_vector(int argc, char **argv);
+  void Merge_sort(std::vector<int> vct);
 
-	class ParseError : public std::exception {
-		public:
-		virtual const char *what () const throw();
-	};
-	class NotSorted : public std::exception {
-		public:
-		virtual const char *what () const throw();
-	};
-	class StartSorted : public std::exception {
-		public:
-		virtual const char *what () const throw();
-	};
+  //helper function
+  bool create_pairs(std::vector<int> &vct) {
+    if (vct.size() < 2)
+        return true;
+
+    for (size_t i = 0; i < vct.size(); i += 2) {
+        if (i + 1 < vct.size()) {
+            // On met TOUJOURS le plus grand en premier
+            if (vct[i] > vct[i + 1])
+                _pairs.push_back(std::make_pair(vct[i], vct[i + 1]));
+            else
+                _pairs.push_back(std::make_pair(vct[i + 1], vct[i]));
+        } else {
+            _stash = vct[i];
+        }
+    }
+    return true;
+  }
+  bool is_sorted(const std::vector<int> &vct) {
+    if (vct.size() < 2)
+      return true;
+    for (size_t i = 1; i < vct.size(); ++i) {
+      if (vct[i - 1] > vct[i]) {
+        _logger << "isn't sorted = " << vct[i - 1] << ">" << vct[i]
+                << std::endl;
+        return false;
+      }
+    }
+    return true;
+  };
+
+  void print_vector() {
+    for (size_t i = 0; i < _v.size(); i++) {
+      _logger << _v[i] << " ";
+      std::cout << _v[i] << " ";
+    }
+    _logger << std::endl;
+    std::cout << std::endl;
+  };
+
+  bool check_duplicate_value(const std::vector<int> &vct) {
+    if (vct.size() < 2)
+      return true;
+    for (size_t i = 1; i < vct.size(); ++i) {
+      if (vct[i - 1] == vct[i]) {
+        _logger << "duplicate value = " << vct[i - 1] << "=" << vct[i]
+                << std::endl;
+        return false;
+      }
+    }
+    return true;
+  };
+
+  template <typename T> void print_time(const T &inspected) {
+    std::cout << "Time to process a range of " << inspected.size()
+              << " elements with "
+              << libftpp::meta::TypeInspector::name(inspected) << " : "
+              << libftpp::time::Clock::elapsed_compact_since_us(_start_time)
+              << std::endl;
+  }
+
+public:
+  PmergeMe(int argv, char **tab);
+  ~PmergeMe() {};
+  PmergeMe(const PmergeMe &other)
+      : _logger(other._logger), _v(other._v), _pairs(other._pairs),
+        _chain_deque(other._chain_deque), _chain_vector(other._chain_vector),
+        _stash(other._stash), _start_time(other._start_time),
+        _end_time(other._end_time) {};
+  PmergeMe &operator=(const PmergeMe &other) {
+    if (this != &other) {
+      _logger = other._logger;
+      _v = other._v;
+      _pairs = other._pairs;
+      _chain_deque = other._chain_deque;
+      _chain_vector = other._chain_vector;
+      _stash = other._stash;
+      _start_time = other._start_time;
+      _end_time = other._end_time;
+    }
+    return *this;
+  }
+
+  libftpp::time::timestamp_us_t get_start_size() const {
+    return this->_start_time;
+  };
+  libftpp::time::timestamp_us_t get_end_time() const {
+    return this->_end_time;
+  };
+  std::vector<int> get_v() const { return this->_v; };
+  std::vector<std::pair<int, int> > get_pairs() const { return this->_pairs; };
+  std::deque<int> get_chain_deque() const { return this->_chain_deque; };
+  std::vector<int> get_chain_vector() const { return this->_chain_vector; };
+  int get_stash() const { return this->_stash; };
+
+  void run();
+
+  class ParseError : public std::exception {
+  public:
+    virtual const char *what() const throw() {
+      return "Parsing error look logs";
+    };
+  };
+  class NotSorted : public std::exception {
+  public:
+    virtual const char *what() const throw() { return "this isn't sorted"; };
+  };
+  class StartSorted : public std::exception {
+  public:
+    virtual const char *what() const throw() { return "It's sorted"; };
+  };
 };
